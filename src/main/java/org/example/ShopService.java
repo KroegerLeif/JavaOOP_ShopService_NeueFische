c1b8af9441aa2a13ab78694eaee0a3e6c616664b
KroegerLeif <Leif.kr@outlook.com>
2025-09-04 09:40:42 +0200

    Shop Service now uses the interface and not the implementation

--- src/main/java/org/example/ShopService.java
+++ src/main/java/org/example/ShopService.java
@@ -5,18 +5,18 @@ import java.util.UUID;
 
 public class ShopService {
     private ProductRepo productRepo;
-    private OrderListRepo orderListRepo;
+    private OrderRepo orderRepo;
 
-    public ShopService(ProductRepo productRepo, OrderListRepo orderListRepo) {
+    public ShopService(ProductRepo productRepo, OrderRepo orderRepo) {
         this.productRepo = productRepo;
-        this.orderListRepo = orderListRepo;
+        this.orderRepo = orderRepo;
     }
 
     public void placeNewOrder(Product product) {
         if(checksStock(product)){
             UUID uuid = UUID.randomUUID();
             Order order = new Order(uuid.toString(),product.price());
-            orderListRepo.addOrder(order);
+            orderRepo.addOrder(order);
         }else {
             System.out.println("Produkt nicht in Stock");
         }
@@ -32,26 +32,26 @@ public class ShopService {
     public ProductRepo getProductRepo() {
         return productRepo;
     }
-    public OrderListRepo getOrderListRepo() {
-        return orderListRepo;
+    public OrderRepo getOrderListRepo() {
+        return orderRepo;
     }
 
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof ShopService that)) return false;
-        return Objects.equals(productRepo, that.productRepo) && Objects.equals(orderListRepo, that.orderListRepo);
+        return Objects.equals(productRepo, that.productRepo) && Objects.equals(orderRepo, that.orderRepo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(productRepo, orderListRepo);
+        return Objects.hash(productRepo, orderRepo);
     }
 
     @Override
     public String toString() {
         return "ShopService{" +
                 "productRepo=" + productRepo +
-                ", orderListRepo=" + orderListRepo +
+                ", orderListRepo=" + orderRepo +
                 '}';
     }
 }
